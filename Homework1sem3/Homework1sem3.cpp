ee038ed75359f306692d4c9944552f35362f415c
Grigoriy6 <grigoriy.6.petrov@gmail.com>
2023-10-23 11:40:39 +0300

    Добавьте файлы проекта.

---
+++ Homework1sem3/Homework1sem3.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class chess_piece {
+public:
+    chess_piece() {
+        cout << "Base class" << endl;
+    }
+    virtual void generate_moves() {
+        cout << "some moves were generated" << endl;
+    }
+
+};
+class pawn : public chess_piece {
+public:
+
+    pawn() {
+        cout << "Derived class" << endl;
+    }
+    void generate_moves() override {
+        cout << "pawn moves were generated" << endl;
+    }
+
+};
+class basic_chess_piece : public chess_piece {
+public:
+    basic_chess_piece() {
+        cout << "Derived class2" << endl;
+    }
+};
+int main() {
+    pawn obj;
+    basic_chess_piece obj2;
+    obj.generate_moves();
+    obj2.generate_moves();
+
+    return 0;
+}
